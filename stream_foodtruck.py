8db7d4764ccfbef9078fedd0537cb16c28025691
Jiaqi Wu <Jiaqi.Wu@ge.com>
2015-12-23 12:10:08 -0800

    Fixed more indentations

--- stream_foodtruck.py
+++ stream_foodtruck.py
@@ -49,61 +49,61 @@ api = tweepy.API(auth)
 
 class StreamHandler(tweepy.StreamListener):
 
-	def on_data(self, data):
-		statusobj = json.loads(unicode(data))
-		_logger.debug("tweet received: " + statusobj['text'])
-		if re.search(BR3_REGEX, statusobj['text']) != None:
-			br3_portion = re.search(r"@?BR#?3\s+(.*)", statusobj['text']).group(1)
-			br3_mentions = re.findall(r"\@([a-zA-Z0-9_]+)", br3_portion)
-			push_message = ""
-	    for mention in br3_mentions:
-			tw_user = api.get_user(mention)
-			push_message += tw_user.name + "\n"
-			if tw_user.url:
-				push_message += tw_user.url + "\n"
-		    else:
-				push_message += "https://twitter.com/" + mention + "\n"
-		_logger.debug("text matched, pushing to Pushbullet Channel")
-		pushbullet_message(push_message, PB_BR3_TAG)
+    def on_data(self, data):
+        statusobj = json.loads(unicode(data))
+        _logger.debug("tweet received: " + statusobj['text'])
+        if re.search(BR3_REGEX, statusobj['text']) != None:
+            br3_portion = re.search(r"@?BR#?3\s+(.*)", statusobj['text']).group(1)
+            br3_mentions = re.findall(r"\@([a-zA-Z0-9_]+)", br3_portion)
+            push_message = ""
+            for mention in br3_mentions:
+                tw_user = api.get_user(mention)
+                push_message += tw_user.name + "\n"
+                if tw_user.url:
+                    push_message += tw_user.url + "\n"
+                else:
+                    push_message += "https://twitter.com/" + mention + "\n"
+            _logger.debug("text matched, pushing to Pushbullet Channel")
+            pushbullet_message(push_message, PB_BR3_TAG)
 
-	def on_error(self, status_code):
-		_logger.error("Error status: " + str(status_code))
-		if status_code == 420:
-			_logger.error("Rate limit hit, sleeping for 1 hour before reconnect")
-			time.sleep(3600)
-			return True
-		else:
-			_logger.error("sleeping for 2 minutes before reconnect")
-			time.sleep(120)
-			return True
-		
+    def on_error(self, status_code):
+        _logger.error("Error status: " + str(status_code))
+        if status_code == 420:
+            _logger.error("Rate limit hit, sleeping for 1 hour before reconnect")
+            time.sleep(3600)
+            return True
+        else:
+            _logger.error("sleeping for 2 minutes before reconnect")
+            time.sleep(120)
+            return True
+        
 
 def pushbullet_message(message, channel_tag):
-	conn = httplib.HTTPSConnection(PUSHBULLET_URL)
-	headers = {"Content-Type": "application/x-www-form-urlencoded",
-	 "Authorization": "Bearer " + PUSHBULLET_TOKEN}
-	params = {"channel_tag": channel_tag, "type": "note", "title": "Food Trucks at BR3 Today", "body": message}
-	params = urllib.urlencode(params)
-	conn.request("POST", "/v2/pushes", params, headers)
-	response = conn.getresponse()
-	if(response.status == httplib.UNAUTHORIZED):
-		sys.exit("Unauthorized")
-	elif(response.status != httplib.OK):
-		sys.exit("Something else went wrong when pushing")
-	conn.close()
+    conn = httplib.HTTPSConnection(PUSHBULLET_URL)
+    headers = {"Content-Type": "application/x-www-form-urlencoded",
+     "Authorization": "Bearer " + PUSHBULLET_TOKEN}
+    params = {"channel_tag": channel_tag, "type": "note", "title": "Food Trucks at BR3 Today", "body": message}
+    params = urllib.urlencode(params)
+    conn.request("POST", "/v2/pushes", params, headers)
+    response = conn.getresponse()
+    if(response.status == httplib.UNAUTHORIZED):
+        sys.exit("Unauthorized")
+    elif(response.status != httplib.OK):
+        sys.exit("Something else went wrong when pushing")
+    conn.close()
 
 
 while(True):
 
-	try:
-		_logger.info("streaming...")
-		stream = tweepy.Stream(auth, StreamHandler())
-		stream.filter(follow=[FOODTRUCK_MAFIA_ID, JERSEY_GIRL_ID])
-		#stream.filter(track=[sys.argv[1]])
-	except KeyboardInterrupt:
-		sys.exit()
-	except Exception, e:
-		logging.exception("error occured")
-		time.sleep(2)
-		_logger.info("starting again...")
-	
+    try:
+        _logger.info("streaming...")
+        stream = tweepy.Stream(auth, StreamHandler())
+        stream.filter(follow=[FOODTRUCK_MAFIA_ID, JERSEY_GIRL_ID])
+        #stream.filter(track=[sys.argv[1]])
+    except KeyboardInterrupt:
+        sys.exit()
+    except Exception, e:
+        logging.exception("error occured")
+        time.sleep(2)
+        _logger.info("starting again...")
+    
